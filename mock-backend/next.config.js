f6f4bc189b03f710bb8007c3e877e4fad40fbc92
Louis Arge <louislva@gmail.com>
2023-01-30 16:33:58 +0100

    out with mock backend

--- mock-backend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  largePageDataBytes: 1024 * 1024 * 250, // 250MB
-}
-
-module.exports = nextConfig
